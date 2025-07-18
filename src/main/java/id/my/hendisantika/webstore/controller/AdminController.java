9504957427ded13d16b4eda530eaa62e65d9bdab
hendisantika <hendisantika@yahoo.co.id>
2025-07-19 05:18:49 +0700

    feat: Add home in AdminController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/webstore/controller/AdminController.java
+++ src/main/java/id/my/hendisantika/webstore/controller/AdminController.java
@@ -1,11 +1,19 @@
 package id.my.hendisantika.webstore.controller;
 
+import id.my.hendisantika.webstore.entity.Category;
+import id.my.hendisantika.webstore.entity.Product;
+import id.my.hendisantika.webstore.entity.User;
 import id.my.hendisantika.webstore.repository.CategoryRepository;
 import id.my.hendisantika.webstore.repository.ProductRepository;
 import id.my.hendisantika.webstore.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -28,4 +36,23 @@ public class AdminController {
     private final ProductRepository productRepo;
 
     private final CategoryRepository categoryRepo;
+
+    @GetMapping(value = { "/", "/home" })
+    public String home(Model m, Principal principal) {
+
+        User user = this.userRepo.loadUserByUserName(principal.getName());
+
+        List<User> users = this.userRepo.getUsers();
+        List<Category> categories = this.categoryRepo.getCategories();
+        List<Product> products = this.productRepo.getProducts();
+
+        m.addAttribute("title", "Admin | StoreWala");
+        m.addAttribute("user", user);
+        m.addAttribute("users", users);
+        m.addAttribute("products", products);
+        m.addAttribute("categories", categories);
+        m.addAttribute("category", new Category());
+
+        return "admin/index";
+    }
 }
