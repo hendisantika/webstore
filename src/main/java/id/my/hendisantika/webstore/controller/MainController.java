e4b75fd3094eb94861278c4a4c0e35204a1b4563
hendisantika <hendisantika@yahoo.co.id>
2025-07-19 05:20:54 +0700

    feat: Add firstHomeView in MainController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/webstore/controller/MainController.java
+++ src/main/java/id/my/hendisantika/webstore/controller/MainController.java
@@ -1,5 +1,7 @@
 package id.my.hendisantika.webstore.controller;
 
+import id.my.hendisantika.webstore.entity.Category;
+import id.my.hendisantika.webstore.entity.Product;
 import id.my.hendisantika.webstore.repository.CategoryRepository;
 import id.my.hendisantika.webstore.repository.CommentRepository;
 import id.my.hendisantika.webstore.repository.ProductRepository;
@@ -8,6 +10,11 @@ import id.my.hendisantika.webstore.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -35,4 +42,20 @@ public class MainController {
     private final CommentRepository commentRepo;
 
     private final BCryptPasswordEncoder passwordEncoder;
+
+    @GetMapping(value = { "/", "/home" })
+    public String firstHomeView(Model m, Principal principal) {
+
+        if (principal != null) {
+            m.addAttribute("user", this.userRepo.loadUserByUserName(principal.getName()));
+        }
+
+        List<Product> products = this.productRepo.getProducts();
+        List<Category> categories = this.categoryRepo.getCategories();
+
+        m.addAttribute("title", "StoreWala | Start Shopping Now!");
+        m.addAttribute("categories", categories);
+        m.addAttribute("products", products);
+        return "index.html";
+    }
 }
