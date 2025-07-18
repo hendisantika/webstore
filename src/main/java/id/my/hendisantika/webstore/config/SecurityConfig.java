fb818247b0f4b413180dd63dd4a416d3d6efca96
hendisantika <hendisantika@yahoo.co.id>
2025-07-19 04:23:20 +0700

    feat: Add filterChain in SecurityConfig🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/webstore/config/SecurityConfig.java
+++ src/main/java/id/my/hendisantika/webstore/config/SecurityConfig.java
@@ -1,12 +1,26 @@
 package id.my.hendisantika.webstore.config;
 
+import id.my.hendisantika.webstore.entity.User;
 import id.my.hendisantika.webstore.repository.UserRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -42,5 +56,81 @@ public class SecurityConfig {
         daoAuthenticationProvider.setUserDetailsService(this.getUserDetailsService());
         daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
         return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/customer/**").hasRole("CUSTOMER")
+                        .requestMatchers("/seller/**").hasRole("SELLER")
+                        .requestMatchers("/**", "/register").permitAll()
+                )
+                .formLogin(form -> form
+                        .successHandler(new AuthenticationSuccessHandler() {
+                            @Override
+                            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                                                Authentication authentication) throws IOException, ServletException {
+
+                                User user = userRepo.loadUserByUserName(authentication.getName());
+
+                                String redirectURL = request.getContextPath();
+
+                                if(user.getRole().equals("ROLE_CUSTOMER")) {
+                                    redirectURL = "/customer/home";
+                                }
+                                if(user.getRole().equals("ROLE_SELLER")) {
+                                    redirectURL = "/seller/home";
+                                }
+                                if(user.getRole().equals("ROLE_ADMIN")) {
+                                    redirectURL = "/admin/home";
+                                }
+
+                                response.sendRedirect(redirectURL);
+                            }
+                        })
+                        .failureHandler(new AuthenticationFailureHandler() {
+                            @Override
+                            public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                                                AuthenticationException exception) throws IOException, ServletException {
+
+                                HttpSession httpSession = request.getSession();
+
+                                if(exception.getMessage().equals("Bad credentials")) {
+                                    httpSession.setAttribute("status", "bad-credentials");
+                                    response.sendRedirect("/login?=BadCredentials");
+                                    return;
+                                }
+
+                                if(exception.getMessage().equals("User is disabled")) {
+                                    httpSession.setAttribute("status", "user-disabled");
+                                    response.sendRedirect("/login?=AccountSuspended");
+                                }
+                            }
+                        })
+                        .loginPage("/login")
+                )
+                .logout(logout -> logout
+                        .logoutSuccessHandler(new LogoutSuccessHandler() {
+                            @Override
+                            public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+                                    throws IOException, ServletException {
+
+                                HttpSession httpSession = request.getSession();
+
+                                if(authentication!=null) {
+                                    httpSession.setAttribute("status", "logout-success");
+                                    response.sendRedirect("/login?logoutSuccess");
+                                }
+                                else {
+                                    response.sendRedirect("/login?doLogin");
+                                }
+                            }
+                        })
+                )
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
 }
