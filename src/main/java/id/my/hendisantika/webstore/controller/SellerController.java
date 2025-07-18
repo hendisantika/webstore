c2dbb01aba0c12a9d1f2beec9fc613d209b73bed
hendisantika <hendisantika@yahoo.co.id>
2025-07-19 05:24:53 +0700

    feat: Add sellerHome in SellerController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/webstore/controller/SellerController.java
+++ src/main/java/id/my/hendisantika/webstore/controller/SellerController.java
@@ -1,12 +1,19 @@
 package id.my.hendisantika.webstore.controller;
 
+import id.my.hendisantika.webstore.entity.Category;
+import id.my.hendisantika.webstore.entity.Product;
+import id.my.hendisantika.webstore.entity.User;
 import id.my.hendisantika.webstore.repository.CategoryRepository;
 import id.my.hendisantika.webstore.repository.ProductRepository;
 import id.my.hendisantika.webstore.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -35,5 +42,20 @@ public class SellerController {
 
         return randomUUID.toString().replaceAll("_", "");
 
+    }
+
+    @GetMapping(value = { "/home", "/" })
+    public String sellerHome(Model m, Principal principal) {
+
+        User user = this.userRepo.loadUserByUserName(principal.getName());
+        List<Product> sellerProducts = this.productRepo.getSellerAllProducts(user.getId());
+        List<Category> categories = this.categoryRepo.getCategories();
+
+        m.addAttribute("title", "Seller Panel | StoreWala");
+        m.addAttribute("product", new Product());
+        m.addAttribute("categories", categories);
+        m.addAttribute("user", user);
+        m.addAttribute("products", sellerProducts);
+        return "seller/index";
     }
 }
