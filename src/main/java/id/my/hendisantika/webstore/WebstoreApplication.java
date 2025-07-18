445d2f67c54f0445bef0aed502d7bc6908c49d3d
hendisantika <hendisantika@yahoo.co.id>
2025-07-18 14:33:30 +0700

    feat: Initial Commit🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/webstore/WebstoreApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.webstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/webstore/WebstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.webstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
