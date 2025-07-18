ced2e199f50ba1bf918276740cce4bbc14de91fb
hendisantika <hendisantika@yahoo.co.id>
2025-07-18 15:00:38 +0700

    feat: Add Comment Entity🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/webstore/entity/Comment.java
@@ -0,0 +1,51 @@
+package id.my.hendisantika.webstore.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : webstore
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 18/07/25
+ * Time: 14.59
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@Entity
+@Table(name = "comments")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private User user;
+
+    @Column(name = "comment_related_to")
+    private int commentRelatedTo;
+
+    @Column(name = "comment")
+    @NotBlank(message = "Comment cannot be empty.")
+    private String comment;
+
+    @Column(name = "comment_added")
+    private java.util.Date date;
+
+}
