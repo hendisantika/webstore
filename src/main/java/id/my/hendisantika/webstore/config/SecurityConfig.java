afaaca370ad011686af574e437ab32dc15c2e1f9
hendisantika <hendisantika@yahoo.co.id>
2025-07-19 04:22:14 +0700

    feat: Add SecurityConfig🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/webstore/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package id.my.hendisantika.webstore.config;
+
+import id.my.hendisantika.webstore.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : webstore
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 19/07/25
+ * Time: 04.21
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private UserRepository userRepo;
+}
